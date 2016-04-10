07040c05757b2ea57690878a44c69a1e1730deae
Warren Jay Eggly <eggzie@gmail.com>
2016-04-09 18:46:32 -0700

    Update raspberrypi.bat

--- qemu/raspberrypi.bat
+++ qemu/raspberrypi.bat
@@ -1,1 +1,33 @@
-qemu-system-arm.exe -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw init=/bin/bash" -hda 2015-02-16-raspbian-wheezy.img
+Rem RaspberryPI QEMU startup script
+
+Rem QUME exe 
+set QEMU=qemu-system-arm.exe
+
+Rem -kernel
+set KERNEL=kernel-qemu
+
+Rem -cpu
+set CPU=arm1176
+
+Rem -M
+set MACHINE=arm1176
+
+Rem -m
+set RAM=256
+
+Rem Image file
+set IMAGE=2016-03-18-raspbian-jessie.img
+
+Rem -serial
+set SERIAL=stdio
+
+Rem disk
+set DISK=/dev/sda2
+
+Rem init
+set INIT=/bin/bash
+
+Rem rootfstype
+set ROOTFSTYPE=ext4
+
+%QEMU% -kernel %KERNEL% -cpu %CPU% -m %RAM% -M %MACHINE% -no-reboot -serial %SERIAL% -append "root=%DISK% panic=1 rootfstype=%ROOTFSTYPE% rw init=%INIT%" -hda %IMAGE%
