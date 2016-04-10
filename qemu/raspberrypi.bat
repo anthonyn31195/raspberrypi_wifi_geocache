3727d3f1617a1b93b6babb133904b642f6e29098
Warren Jay Eggly <eggzie@gmail.com>
2016-04-09 18:26:57 -0700

    Create raspberrypi.bat

---
+++ qemu/raspberrypi.bat
@@ -0,0 +1,1 @@
+qemu-system-arm.exe -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw init=/bin/bash" -hda 2015-02-16-raspbian-wheezy.img
