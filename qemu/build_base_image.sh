ae5f753e223b6ccbddb30067625731f949ab7b40
Warren Jay Eggly <eggzie@gmail.com>
2016-04-19 08:03:26 -0700

    update files

--- qemu/build_base_image.sh
+++ qemu/build_base_image.sh
@@ -1,13 +1,55 @@
 #!/bin/bash -xv
 
 IMAGE_DIR=../image
-ZIP_IMAGE=`ls -rt $IMAGE_DIR/*.zip | tail -1`
+
+ZIP_IMAGE=../../2016-03-18-raspbian-jessie.zip
+
 IMAGE=$IMAGE_DIR/`basename $ZIP_IMAGE .zip`.img
+
 QCOW2_IMAGE=$IMAGE_DIR/`basename $ZIP_IMAGE .zip`.qcow2
-if [ ! -f "$IMAGE" ] ; then
-  unzip -d $IMAGE_DIR $ZIP_IMAGE
+
+MOUNT=/tmp/`basename $ZIP_IMAGE .zip`
+
+IMAGE_SIZE=+10G
+
+if [ ! -d $IMAGE_DIR ] ; then
+  mkdir $IMAGE_DIR
 fi
 
+if [ ! -d "$MOUNT" ] ; then
+  mkdir $MOUNT
+fi
+
+if [ ! -f "$IMAGE" ] ; then
+  unzip -d $IMAGE_DIR $ZIP_IMAGE
+  #qemu-img resize $IMAGE $IMAGE_SIZE
+fi
+
+if [ -f "$IMAGE" ] ; then
+  OFFSITE=`fdisk -l $IMAGE | awk '/img2/ {print $2*512}'`
+fi
+
+if [ ! -d "$MOUNT" ] ; then
+  mkdir $MOUNT
+fi
+
+if [ -n "$OFFSITE" ] ; then
+  sudo mount -o loop,offset=$OFFSITE $IMAGE $MOUNT
+fi
+
+sudo sed -i 's/^/#/' $MOUNT/etc/ld.so.preload
+
+
+cat > /tmp/90-qemu.rules <<EOD
+KERNEL=="sda", SYMLINK+="mmcblk0"
+KERNEL=="sda?", SYMLINK+="mmcblk0p%n"
+KERNEL=="sda2", SYMLINK+="root"
+EOD
+sudo mv /tmp/90-qemu.rules $MOUNT/etc/udev/rules.d/90-qemu.rules
+
+sudo umount $MOUNT
+
+exit
 if [ ! -f "$QCOW2_IMAGE" ] ; then
   qemu-img convert -f raw -O qcow2 $IMAGE $QCOW2_IMAGE
 fi
--- qemu/raspberrypi.sh
+++ qemu/raspberrypi.sh
@@ -1,10 +1,25 @@
 #!/bin/bash -xv
+KERNEL_DIR=../../qemu-rpi-kernel
+KERNEL=kernel-qemu-4.1.13-jessie
+IMAGE=2016-03-18-raspbian-jessie.img
 IMAGE_DIR=../image
-qemu-system-arm -kernel $IMAGE_DIR/kernel-qemu-4.1.13-jessie \
+IMAGE_FORMAT=raw
+
+if [ ! -f "$KERNEL_DIR/$KERNEL" ] ; then
+  exit
+fi
+
+if [ ! -f "$IMAGE_DIR/$IMAGE" ] ; then
+  exit
+fi
+
+qemu-system-arm -kernel $KERNEL_DIR/$KERNEL \
   -cpu arm1176 \
   -m 256 \
   -M versatilepb \
   -no-reboot \
-  -serial stdio \
-  -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw init=/bin/bash" \
-  -drive file=$IMAGE_DIR/2016-03-18-raspbian-jessie.qcow2,index=0,media=disk,format=qcow2
+  -append "root=/dev/sda2 panic=0 rootfstype=ext4 rw init=/bin/bash" \
+  -hda $IMAGE_DIR/$IMAGE
+
+#  -serial stdio \
+#  -append "root=/dev/sda2 panic=0 rootfstype=ext4 rw init=/bin/bash" \
