cd63cb885879ea1d6bba68023336b864e3bea717
Warren Jay Eggly <eggzie@gmail.com>
2016-04-20 21:05:47 -0700

    updateing raspberrypi2.sh

--- qemu/raspberrypi2.sh
+++ qemu/raspberrypi2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-IMAGE_DIR=../image
+IMAGE_DIR=../../RaspberryPI
 image_base=2016-03-18-raspbian-jessie.qcow2
 image=$IMAGE_DIR/$image_base
 kernel=$IMAGE_DIR/kernel-qemu-4.1.13-jessie
@@ -7,13 +7,19 @@ format=raw
 format=qcow2
 date=`date +%Y-%m-%d-%H-%M-%S`
 snapshot_dir=snapshot
-snapshot=$snapshot_dir/$image_base-$date
+snapshot=$image_base-$date
 
 if [ ! -d "$snapshot_dir" ] ; then
   mkdir $snapshot_dir
 fi
 
-qemu-img create -f $format -b ../$image $snapshot 
+if [ -f "$image" ] ; then
+  (cd $snapshot_dir ; qemu-img create -f $format -b ../$image $snapshot > /dev/null 2>&1 )
+fi
+
+if [ ! -f $snapshot_dir/$snapshot ] ; then
+  exit
+fi 
 
 qemu-system-arm -kernel $kernel \
   -cpu arm1176 \
@@ -23,9 +29,9 @@ qemu-system-arm -kernel $kernel \
   -serial stdio \
   -snapshot \
   -append "root=/dev/sda2 panic=1 -no-reboot rootfstype=ext4 rw format=$format" \
-  -drive file="$snapshot",index=0,media=disk,format=$format \
+  -drive file="$snapshot_dir/$snapshot",index=0,media=disk,format=$format \
   -net nic \
   -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::22280-:80 \
-  -pidfile raspberrypi.pid 
+  -pidfile raspberrypi.pid > /dev/null 2>&1 &
 #  -daemonize
 #  -serial stdio \
