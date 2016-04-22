f620c0a7a5e9582d895500f953635486dc61a535
Warren Jay Eggly <eggzie@gmail.com>
2016-04-21 23:06:26 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Setup script config a raspberryPI 
+sudo raspi-config
 sudo apt-get --quiet update
 sudo apt-get --yes --quiet upgrade
-sudo apt-get --yes --quiet install dnsmasq dnsutils hostapd iptables lighttpd php5-cgi 
+sudo apt-get --yes --quiet install dnsmasq dnsutils hostapd iptables lighttpd php5-cgi openssl
+apt-get --yes autoremove
