12d0db052859c3e1e7e34e78f8bfdf04a820a6ea
Warren Jay Eggly <eggzie@gmail.com>
2016-04-22 15:08:14 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 sudo apt-get --quiet update
 sudo apt-get --yes --quiet upgrade
 sudo apt-get --yes --quiet install dnsmasq dnsutils hostapd iptables lighttpd php5-cgi 
+
+/etc/lighttpd/conf-enabled
+ls -s ../conf-available/15-fastcgi-php.conf
+ln -s ../conf-available/10-fastcgi.conf
