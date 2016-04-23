391bb977022a6a68d871e0e971a0bed75425654b
Warren Jay Eggly <eggzie@gmail.com>
2016-04-23 06:40:32 -0700

    updating files

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<tile>Splasher Hacker CacheBerry</title>
+</head>
+<body>
+	<h2>Splasher Hacker CacheBerry</h2>
+	<h2>Find Code: CacheBerry</h2>
+	<h2>Trackme at Geocaching.com</h2>
+	<h2>Tacking Code: XYZ</h2>
+</body>
+</html>
