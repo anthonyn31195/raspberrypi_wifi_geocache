1d4c9467c60d0317822685dde37171ceea74a197
Warren Jay Eggly <eggzie@gmail.com>
2016-04-29 05:54:24 -0700

    updating startweb.sh

--- web/startweb.sh
+++ web/startweb.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 PHP=/usr/bin/php
 PORT=8000
-PID_FILE=/tmp/php.pid
+PID_FILE=/tmp/`basename $0 .sh`.pid
+LOG_FILE=/tmp/`basename $0 .sh`.log
 OS=`uname`
 if [ -f "$PID_FILE" ] ; then
   pkill -F $PID_FILE
 fi
 DIR=`dirname $0`
 
-if [ -f "$PID_FILE"] ; then 
+if [ -f "$PID_FILE" ] ; then 
   /bin/rm $PID_FILE
 fi
 
-eth0=`ifconfig eth0 | awk '/inet / {print $2}' 2>/dev/null `
-lo0=`ifconfig lo0 | awk '/inet / {print $2}' 2>/dev/null `
+echo "starting php" >> $LOG_FILE
+
+eth0=`ifconfig eth0 | awk '/inet / {print $2}' | sed 's/addr://' 2>/dev/null `
+lo0=`ifconfig lo0 | awk '/inet / {print $2}' | sed 's/addr://' 2>/dev/null `
 if [ -n "$lo0" ] ; then
-  $PHP -S $lo0:$PORT -t $DIR > /dev/null 2>&1 & 
+  $PHP -S $lo0:$PORT -t $DIR >> $LOG_FILE 2>&1 & 
   echo $! >> $PID_FILE
 fi
 if [ -n "$eth0" ] ; then
-  $PHP -S $eth0:$PORT -t $DIR > /dev/null 2>&1 &
+  $PHP -S $eth0:$PORT -t $DIR >> $LOG_FILE 2>&1 &
   echo $! >> $PID_FILE
 fi
