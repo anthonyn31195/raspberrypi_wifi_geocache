c63f55ff18ec9c416bf068dd39e2e184434c1674
Warren Jay Eggly <eggzie@gmail.com>
2016-05-03 08:40:59 -0700

    update about

--- web/about.php
+++ web/about.php
@@ -16,7 +16,7 @@ include_once(dirname(__file__) . "/lib/header.php");
 
   <div class="w3-row">
     <div class="w3-col s3">
-      <center><img src="/image/dnsmasq_logo.png" width="66px"></center>
+      <center><img src="/image/dnsmasq_logo.png" width="96px"></center>
     </div>
     <div class="w3-col s9 w3-container">
       <h3>Dnsmasq</h3>
@@ -36,7 +36,17 @@ include_once(dirname(__file__) . "/lib/header.php");
 
   <div class="w3-row">
     <div class="w3-col s3">
-      <center><img src="<?php print raspberry_pi_logo ?>" width="66px"></center>
+      <center><img src="/image/raspbian.png" width="96px"></center>
+    </div>
+    <div class="w3-col s9 w3-container">
+      <h3>Raspbian</h3>
+    </div>
+  </div>
+<hr>
+
+<div class="w3-row">
+    <div class="w3-col s3">
+      <center><img src="<?php print raspberry_pi_logo ?>" width="56px"><br>Raspbian</center>
     </div>
     <div class="w3-col s9 w3-container">
       <h3><?php print raspberry_pi_name; ?></h3>
@@ -47,7 +57,7 @@ include_once(dirname(__file__) . "/lib/header.php");
 
   <div class="w3-row">
     <div class="w3-col s3">
-      <center><img src="/image/Wi-Fi-dk-green-cube.png" width="66px"></center>
+      <center><img src="/image/WiFi-hotspot.png" width="66px"></center>
     </div>
     <div class="w3-col s9 w3-container">
       <h3>Wi-FI</h3>
