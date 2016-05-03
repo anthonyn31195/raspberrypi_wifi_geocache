ae77320d681f98862bcc000ab8f15ed8406cb1b1
Warren Jay Eggly <eggzie@gmail.com>
2016-05-03 08:21:23 -0700

    update about

--- web/about.php
+++ web/about.php
@@ -3,16 +3,58 @@
 include_once(dirname(__file__) . "/lib/header.php");
 ?>
 
+<div class="w3-row">
+    <div class="w3-col s3">
+      <center><a href="/"><img src="<?php print image ?>"></a></center>
+    </div>
+    <div class="w3-col s9 w3-container">
+      <h3><?php print cache_name; ?></h3>
+    </div>
+  </div>
+
+<hr>
 
   <div class="w3-row">
     <div class="w3-col s3">
-      <img src="<?php print raspberry_pi_logo ?>" width="66px">
+      <center><img src="/image/dnsmasq_logo.png" width="66px"></center>
+    </div>
+    <div class="w3-col s9 w3-container">
+      <h3>Dnsmasq</h3>
+    </div>
+  </div>
+<hr>
+
+  <div class="w3-row">
+    <div class="w3-col s3">
+      <center><img src="/image/249px-Lighttpd_logo.svg.png" width="66px"></center>
+    </div>
+    <div class="w3-col s9 w3-container">
+      <h3>Lighttpd</h3>
+    </div>
+  </div>
+<hr>
+
+  <div class="w3-row">
+    <div class="w3-col s3">
+      <center><img src="<?php print raspberry_pi_logo ?>" width="66px"></center>
     </div>
     <div class="w3-col s9 w3-container">
       <h3><?php print raspberry_pi_name; ?></h3>
     </div>
   </div>
 <hr>
+
+
+  <div class="w3-row">
+    <div class="w3-col s3">
+      <center><img src="/image/Wi-Fi-dk-green-cube.png" width="66px"></center>
+    </div>
+    <div class="w3-col s9 w3-container">
+      <h3>Wi-FI</h3>
+    </div>
+  </div>
+<hr>
+
 <?php 
 include_once(dirname(__file__) . "/lib/footer.php");
 ?>
